e462ba176d9ee6694dbdcbab0a1e101772a2e4ef
SushantshtL0L <shtsushant2027@gmail.com>
2025-02-12 23:21:19 +0545

    Pulling

--- app/src/main/java/com/example/jhigu_fitness/ui/activity/ui/activity/AbsActivity.kt
+++ app/src/main/java/com/example/jhigu_fitness/ui/activity/ui/activity/AbsActivity.kt
@@ -1,22 +1,26 @@
-package com.example.jhigu_fitness
+package com.example.jhigu_fitness.ui.activity.ui.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.jhigu_fitness.databinding.ActivityMainBinding
+import com.example.jhigu_fitness.R
+import com.example.jhigu_fitness.databinding.ActivityAbs2Binding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class AbsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAbs2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAbs2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.cardAbs
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/jhigu_fitness/ui/activity/ui/activity/ShoulderActivity.kt
+++ app/src/main/java/com/example/jhigu_fitness/ui/activity/ui/activity/ShoulderActivity.kt
@@ -1,22 +1,17 @@
-package com.example.jhigu_fitness
+package com.example.jhigu_fitness.ui.activity.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.jhigu_fitness.databinding.ActivityMainBinding
+import com.example.jhigu_fitness.R
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class ShoulderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.cardAbs
+        setContentView(R.layout.activity_shoulder)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
